c1570157b8d62ebd95fa27da3e1a3bbac9162651
Daniel Van Der Ploeg <daniel.vanderploeg@aligent.com.au>
2022-06-06 14:43:06 +0930

    Update comments in test file

--- packages/cloudfront-security-headers/test/cdk-cloudfront-security-headers.test.ts
+++ packages/cloudfront-security-headers/test/cdk-cloudfront-security-headers.test.ts
@@ -3,14 +3,12 @@ import * as cdk from 'aws-cdk-lib/core';
 import { SecurityHeaderFunction } from '../lib/index';
 
 /*
- * Example test
+ *  Test the lambda function was created
  */
 test('Lambda Function Created', () => {
   const app = new cdk.App();
   const stack = new cdk.Stack(app, "TestStack", {env: {region: 'us-east-1'}});
-  // WHEN
   new SecurityHeaderFunction(stack, 'MyTestConstruct');
-  // THEN
   const template = Template.fromStack(stack);
   template.resourceCountIs("AWS::Lambda::Function",1);
 });
