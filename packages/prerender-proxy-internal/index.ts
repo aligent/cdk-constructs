e5f9dffc9bf1c790731dd3593d55c430aa5efacb
Daniel Van Der Ploeg <daniel.vanderploeg@aligent.com.au>
2022-07-20 10:14:53 +0930

    Remove prerender-proxy-internal

--- packages/prerender-proxy-internal/index.ts
+++
@@ -1,6 +0,0 @@
-import { PrerenderLambda } from "./lib/prerender-lambda-construct";
-import { PrerenderFunction } from "./lib/prerender-construct";
-import { PrerenderCheckFunction } from "./lib/prerender-check-construct";
-import { ErrorResponseFunction } from "./lib/error-response-construct";
-
-export { PrerenderLambda, PrerenderFunction, PrerenderCheckFunction, ErrorResponseFunction };
