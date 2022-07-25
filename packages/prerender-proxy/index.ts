31d12230913e193917953c6dca0bf5b392628af1
Krishan Thisera <krishan.thisera@aligent.com.au>
2022-07-26 08:56:17 +0930

    Export CloudFrontCacheControlOptions

--- packages/prerender-proxy/index.ts
+++ packages/prerender-proxy/index.ts
@@ -2,6 +2,13 @@ import { PrerenderLambda } from "./lib/prerender-lambda-construct";
 import { PrerenderFunction } from "./lib/prerender-construct";
 import { PrerenderCheckFunction } from "./lib/prerender-check-construct";
 import { ErrorResponseFunction } from "./lib/error-response-construct";
-import { CloudFrontCacheControl } from "./lib/prerender-cf-cache-control-construct"
+import { CloudFrontCacheControl, CloudFrontCacheControlOptions } from "./lib/prerender-cf-cache-control-construct"
 
-export { PrerenderLambda, PrerenderFunction, PrerenderCheckFunction, ErrorResponseFunction, CloudFrontCacheControl };
+export {    
+            PrerenderLambda, 
+            PrerenderFunction, 
+            PrerenderCheckFunction, 
+            ErrorResponseFunction, 
+            CloudFrontCacheControl, 
+            CloudFrontCacheControlOptions 
+        };
