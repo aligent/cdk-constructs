a650f414ff6a13257a338797ce67e6e059666daf
Krishan Thisera <krishan.thisera@aligent.com.au>
2022-11-14 16:25:43 +1030

    Export prerender check function

--- packages/prerender-proxy/index.ts
+++ packages/prerender-proxy/index.ts
@@ -2,14 +2,17 @@ import { PrerenderLambda } from "./lib/prerender-lambda-construct";
 import { PrerenderFunction } from "./lib/prerender-construct";
 import { PrerenderCheckFunction } from "./lib/prerender-check-construct";
 import { ErrorResponseFunction } from "./lib/error-response-construct";
-import { CloudFrontCacheControl, CloudFrontCacheControlOptions } from "./lib/prerender-cf-cache-control-construct"
-import { handler as PrerenderCheckFunctionHandler} from "./lib/handlers/prerender-check"
-export {    
-            PrerenderLambda, 
-            PrerenderFunction, 
-            PrerenderCheckFunction, 
-            ErrorResponseFunction, 
-            CloudFrontCacheControl, 
-            CloudFrontCacheControlOptions,
-            PrerenderCheckFunctionHandler
-        };
+import {
+  CloudFrontCacheControl,
+  CloudFrontCacheControlOptions,
+} from "./lib/prerender-cf-cache-control-construct";
+import { handler as PrerenderCheckFunctionHandler } from "./lib/handlers/prerender-check";
+export {
+  PrerenderLambda,
+  PrerenderFunction,
+  PrerenderCheckFunction,
+  ErrorResponseFunction,
+  CloudFrontCacheControl,
+  CloudFrontCacheControlOptions,
+  PrerenderCheckFunctionHandler,
+};
