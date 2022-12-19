537bd5b7eace409c168fa62689b7e25e0b59a395
Krishan Thisera <krishan.thisera@aligent.com.au>
2022-12-19 17:14:46 +1030

    DO_1419: fixed a syntax issue

--- packages/static-hosting/lib/static-hosting.ts
+++ packages/static-hosting/lib/static-hosting.ts
@@ -363,6 +363,7 @@ export class StaticHosting extends Construct {
                     )};
                 });
             });
+        }
             
         if (publisherGroup) {
             const cloudFrontInvalidationPolicyStatement = new PolicyStatement({
