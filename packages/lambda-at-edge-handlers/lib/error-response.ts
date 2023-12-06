5a1aa7b5282127f51f3330f65f4acbf7ee2db05e
Daniel Van Der Ploeg <daniel.vanderploeg@aligent.com.au>
2023-12-06 13:54:34 +1030

    chore: resolve linting errors

--- packages/lambda-at-edge-handlers/lib/error-response.ts
+++ packages/lambda-at-edge-handlers/lib/error-response.ts
@@ -37,7 +37,7 @@ export const handler = (
     // Fetch default page and return body
     return instance
       .get(`https://${FRONTEND_HOST}${PATH_PREFIX}/index.html`)
-      .then(res => {
+      .then(_res => {
         // Commenting this as there is body is not defined in the CloudFrontResponse type
         // response.body = res.data;
 
@@ -53,7 +53,7 @@ export const handler = (
 
         return response;
       })
-      .catch(err => {
+      .catch(_err => {
         return response;
       });
   }
