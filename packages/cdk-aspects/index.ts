6d03e8bdd3fce4ce433edf87ed3ece594d60fb75
ryanrixxh <ryanmbr@protonmail.com>
2025-12-04 16:02:24 +1030

    restructure and adding aspects

---
+++ packages/cdk-aspects/index.ts
@@ -0,0 +1,4 @@
+import { MicroserviceChecks } from "./microservice-checks/microservice-checks";
+import { VersionFunctionsAspect } from "./version-functions/version-functions";
+
+export { MicroserviceChecks, VersionFunctionsAspect };
