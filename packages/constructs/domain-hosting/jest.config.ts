009eaf42122b3a4ffb2d0d24536d674d920d2d57
ryanrixxh <ryanmbr@protonmail.com>
2026-01-21 10:48:57 +1030

    domain hosting construct move fix after merge

--- packages/constructs/domain-hosting/jest.config.ts
+++ packages/constructs/domain-hosting/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: "domain-hosting",
-  preset: "../../jest.preset.js",
+  preset: "../../../jest.preset.js",
   testEnvironment: "node",
   transform: {
     "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
   },
   moduleFileExtensions: ["ts", "js", "html"],
-  coverageDirectory: "../../coverage/packages/domain-hosting",
+  coverageDirectory: "../../../coverage/packages/construdomain-hosting",
 };
