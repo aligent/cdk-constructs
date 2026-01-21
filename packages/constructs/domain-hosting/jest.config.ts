5989f60fc0b6020b634778ef1332fc7f052eb0f2
Ryan <82134428+ryanrixxh@users.noreply.github.com>
2026-01-21 11:36:48 +1030

    Update packages/constructs/domain-hosting/jest.config.ts
    Yes I did haha

--- packages/constructs/domain-hosting/jest.config.ts
+++ packages/constructs/domain-hosting/jest.config.ts
@@ -7,5 +7,5 @@ export default {
     "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
   },
   moduleFileExtensions: ["ts", "js", "html"],
-  coverageDirectory: "../../../coverage/packages/construdomain-hosting",
+  coverageDirectory: "../../../coverage/packages/constructs/domain-hosting",
 };
